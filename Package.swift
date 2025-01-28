71904562663009dc807c5fd4fad80e62bafdde98
Mx-Iris <61279231+Mx-Iris@users.noreply.github.com>
2025-01-28 22:33:12 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "CXTest", targets: ["CXTest"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/cx-org/CXShim", .upToNextMinor(from: "0.4.0")),
+        .package(url: "https://github.com/MxIris-LyricsX-Project/CXTest", .branchItem("master")),
     ],
     targets: [
         .target(name: "CXTest", dependencies: ["CXShim"]),
@@ -24,11 +24,7 @@ enum CombineImplementation {
     case openCombine
     
     static var `default`: CombineImplementation {
-        #if canImport(Combine)
-        return .combine
-        #else
         return .combineX
-        #endif
     }
     
     init?(_ description: String) {
